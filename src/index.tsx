be9ab7bf267851c6cd31292add06b9ad5aa3bd49
NoellaAbraham <abrahamnoella05@gmail.com>
2025-08-05 14:39:15 +0400

    testing

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/dh-app">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>
