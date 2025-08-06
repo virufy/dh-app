e395b0faa1e99728e69c74b677b46a84a1a427a2
NoellaAbraham <abrahamnoella05@gmail.com>
2025-08-07 00:25:10 +0400

    updated form links in consent page

--- src/containers/App/ConsentPage.tsx
+++ src/containers/App/ConsentPage.tsx
@@ -183,8 +183,8 @@ const ConsentScreen: React.FC = () => {
                             <a
                                 href={
                                         i18n.language === 'ar'
-                                            ? 'https://drive.google.com/file/d/1ZStVIFr7hixosQufTu_ljg2XyuNvmfd_/view'
-                                            : 'https://drive.google.com/file/d/1RZfqtWyiYkSChm0-5raCIbH4zaqL-cqs/view'
+                                            ? 'https://drive.google.com/file/d/1Ba5ywnq6A1LcJehiOSRAr2f1yRIxKLo4/view'
+                                            : 'https://drive.google.com/file/d/1FUmtRxEVgQG2kvUK9uv4m3Vl4OF_rslq/view'
                                             
                                     }
                                 target="_blank"
@@ -198,8 +198,8 @@ const ConsentScreen: React.FC = () => {
                                 <iframe 
                                     src={
                                         i18n.language === 'ar'
-                                            ? 'https://drive.google.com/file/d/1ZStVIFr7hixosQufTu_ljg2XyuNvmfd_/preview'
-                                            : 'https://drive.google.com/file/d/1RZfqtWyiYkSChm0-5raCIbH4zaqL-cqs/preview'
+                                            ? 'https://drive.google.com/file/d/1Ba5ywnq6A1LcJehiOSRAr2f1yRIxKLo4/preview'
+                                            : 'https://drive.google.com/file/d/1FUmtRxEVgQG2kvUK9uv4m3Vl4OF_rslq/preview'
                                     }
                                     width="100%"
                                     height="100%"
@@ -217,7 +217,7 @@ const ConsentScreen: React.FC = () => {
                         <a
                             href={
                                     i18n.language === 'ar'
-                                        ? 'https://drive.google.com/file/d/1Lw_1bQsRbYbTgtocGiFXkQAixlWR-2cR/view'
+                                        ? 'https://drive.google.com/file/d/1VvW2WO2hi0K84yqD6ayna9iBeyzm0X5k/view'
                                         : 'https://drive.google.com/file/d/1hnxvDJ5qHBnUi7cnkNdyD4PuWMz8Ntss/view'
                                 }
                             target="_blank"
@@ -230,7 +230,7 @@ const ConsentScreen: React.FC = () => {
                         <div style={{...iframeStyle, marginTop: '1rem'}}>
                             <iframe
                                 src = {i18n.language === 'ar'
-                                        ? 'https://drive.google.com/file/d/1Lw_1bQsRbYbTgtocGiFXkQAixlWR-2cR/preview'
+                                        ? 'https://drive.google.com/file/d/1VvW2WO2hi0K84yqD6ayna9iBeyzm0X5k/preview'
                                         : 'https://drive.google.com/file/d/1hnxvDJ5qHBnUi7cnkNdyD4PuWMz8Ntss/preview'
                                 }
                                 width="100%"
