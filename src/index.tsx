cf687f930b8a1f9b2c8f87437e9cfbfe65d1567c
NoellaAbraham <abrahamnoella05@gmail.com>
2025-08-13 14:03:02 +0400

    fixes regarding index.tsx routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,27 @@
-import './i18n';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './containers/App/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import './assets/fonts/font.css';
+import "./i18n";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./containers/App/App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "./assets/fonts/font.css";
 
 // Utils
-import swConfig from './utils/swConfig';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import swConfig from "./utils/swConfig";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
-// Always use /dh-app for both dev and prod
-const basename = '/dh-app';
+/**
+ * Use /dh-app when the current URL starts with it (e.g., .../dh-app/*),
+ * otherwise use root (/). Also allow an optional override via REACT_APP_BASENAME.
+ */
+const basename =
+  process.env.REACT_APP_BASENAME ||
+  (window.location.pathname.startsWith("/dh-app") ? "/dh-app" : "/");
 
 root.render(
   <React.StrictMode>
@@ -28,7 +33,7 @@ root.render(
 
 reportWebVitals();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   serviceWorkerRegistration.unregister();
 } else {
   serviceWorkerRegistration.register(swConfig);
